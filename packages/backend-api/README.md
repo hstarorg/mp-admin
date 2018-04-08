b5acca3a9bad9a10d238369db1c501b6c204422c
Hstarorg <hm910705@163.com>
2018-04-08 23:33:03 +0800

    Add local config support

--- packages/backend-api/README.md
+++ packages/backend-api/README.md
@@ -7,3 +7,10 @@ npm start
 # or
 npm run dev
 ```
+
+# 重要说明
+
+为了保证代码规范一致，请遵从如下规则：
+
+1. 使用 `VS Code` 作为默认代码编辑器
+2. 安装 `EditorConfig, Prettier, sort-imports, eslint` 等四个必备插件（项目中以配置上相关格式）
--- packages/backend-api/src/config/index.ts
+++ packages/backend-api/src/config/index.ts
@@ -1,7 +1,9 @@
 import * as path from 'path';
+import localConfig from './local-config';
 
 export default {
   port: 9000,
   routesPath: path.join(__dirname, 'routes'),
-  wxToken: 'hello,wx'
+  wxToken: 'hello,wx',
+  ...localConfig
 };
